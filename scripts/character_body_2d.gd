b1df5d3d259964edbb13c9dc7741b01237ac57ca
pkemmo4 <pkemmo4@users.noreply.github.com>
2025-01-28 00:42:36 +1100

    basic version of time slow implemented; still ironing out errors

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,27 +1,32 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var time_slow_length = $timeSlowLength
 
 var alive = true
-const SPEED = 200
-const JUMP_VELOCITY = -300.0
+var speed = 200
+var jumpVelocity = -300.0
 const dashDistance = 40
 
+func player():
+	pass #function check whether a body is the player
 func _physics_process(delta: float) -> void:
 	# Add gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
 	# perhaps not the best practice slamming it all into one if statement
+	# potentially could use a block statement to check if player is alive at start & if not pass
 	if alive == true:
 		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
+			velocity.y = jumpVelocity
 		var direction := Input.get_axis("move_left", "move_right")
 		move(direction)
 		flip(direction)
 		if Input.is_action_just_pressed("dash"):
 			dash(direction)
-			
+		if Input.is_action_just_pressed("time_slow"): #&& time_slow_length.timeout()
+			time_slow()
 	elif alive == false:
 		velocity.x = 0
 		animated_sprite.modulate = Color(1,0.33,0.33,1)
@@ -29,18 +34,31 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 
-func player():
-	pass #function check whether a body is the player
 func move(direction):
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 func flip(direction):
 	if direction < 0:
 		animated_sprite.flip_h = true
 	elif direction > 0:
 		animated_sprite.flip_h = false
-		
 func dash(direction):
 	position.x += dashDistance * direction
+# Allows the player to slow down time to assist with dodging 
+# bullets while also allowing the player to make it through
+# time slow required sections
+func time_slow():
+	Engine.time_scale = 0.2
+	# not quite as fast as normal
+	speed *= 4
+	# currently has moon gravity
+	jumpVelocity *= 2
+	time_slow_length.start() # might need to start on initialise
+	
+
+func _on_time_slow_length_timeout():
+	Engine.time_scale = 1
+	speed /= 4
+	jumpVelocity /= 2
