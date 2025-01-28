72cb8867b65f79f50aa14e7ef4b2168e652b8630
pkemmo4 <pkemmo4@users.noreply.github.com>
2025-01-28 12:42:07 +1100

    prototyping lighting and reworking turret to burst instead of consistently firing

--- scripts/turret.gd
+++ scripts/turret.gd
@@ -3,16 +3,21 @@ extends Node2D
 @onready var barrel = $Barrel
 @onready var marker_2d = $BulletSpawn/Marker2D
 @onready var barrel_2 = $Barrel/Barrel2
-@onready var timer_fire_rate = $fireRate
+@onready var fire_rate = $fireRate
+@onready var point_light_2d = $PointLight2D
+@onready var explosion_length = $explosionLength
+@onready var reload_speed = $reloadSpeed
 
 const bulletPath = preload("res://Scenes/bullet.tscn")
 var bodyEntered = false
 var newBody
 var canFire = true
+var burstAmount = 3
+var burstCounter = 0
 func _process(delta):
 	if bodyEntered == true:
 		rotateTowards(barrel, newBody.position)
-		if canFire:
+		if canFire == true:
 			fire()
 
 func _on_area_2d_body_entered(body):
@@ -26,7 +31,10 @@ func _on_area_2d_body_exited(body):
 # need to delete bullets; after hitting something
 func fire():
 	canFire = false
-	timer_fire_rate.start()
+	burstCounter += 1
+	if burstCounter != 3:
+		print("fireRate.start()")
+		fire_rate.start()
 	var bullet = bulletPath.instantiate() # create a new instance of a bullet
 	add_child(bullet) # add the bullet as a child of turret
 	bullet.global_position = marker_2d.global_position
@@ -35,10 +43,28 @@ func fire():
 	rotateTowards(bullet, newBody.position)
 	bullet.velocity = newBody.global_position - bullet.global_position # find directional vector towards player
 	barrel_2.play("fire")
+	point_light_2d.enabled = true
+
+
 
 func rotateTowards(object, position): # position is a Vector2
 	object.look_at(position) # default look_at() is 90 degrees off
 	object.rotate(3*TAU/4) # adjust with TAU == 2pi radians
 
 func _on_fire_rate_timeout():
+	print(burstCounter)
+	if burstCounter < 3:
+		canFire = true
+	else:
+		print("timer")
+		reload_speed.start()
+		#canFire = false
+	print(canFire)
+	print()
+func _on_reload_speed_timeout():
+	burstCounter = 0
+	print("reset")
 	canFire = true
+
+func _on_explosion_length_timeout():
+	point_light_2d.enabled = false
