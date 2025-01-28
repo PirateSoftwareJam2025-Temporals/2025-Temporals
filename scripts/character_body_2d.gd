9b42148253fba621cb7d96c2e1ef4aa68cb20121
pkemmo4 <pkemmo4@users.noreply.github.com>
2025-01-28 18:40:25 +1100

    moved some files around and added running animation to player amongst other things

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var time_slow_length = $timeSlowLength
 
 var alive = true
 const DEFAULT_SPEED = 200
@@ -10,7 +9,6 @@ const dashSpeed = 400
 const dashDist = 100
 var dashing = false
 var dashStart = 0
-var timeSlowAvailable = true
 var speed = DEFAULT_SPEED
 func player():
 	pass #function check whether a body is the player
@@ -34,10 +32,13 @@ func _physics_process(delta: float) -> void:
 		var direction := Input.get_axis("move_left", "move_right")
 		move(direction)
 		flip(direction)
+		animated_sprite.play("running")
 		if Input.is_action_just_pressed("dash") && direction:
 			dash(direction)
-		if Input.is_action_just_pressed("time_slow") && timeSlowAvailable: #&& time_slow_length.timeout()
-			time_slow()
+		if Input.is_action_just_pressed("time_slow"): #&& time_slow_length.timeout()
+			Engine.time_scale = 0.2
+		elif Input.is_action_just_released("time_slow"):
+			Engine.time_scale = 1
 	elif alive == false:
 		velocity.x = 0
 		animated_sprite.modulate = Color(1,0.33,0.33,1)
@@ -51,21 +52,15 @@ func move(direction):
 		velocity.x = move_toward(velocity.x, 0, DEFAULT_SPEED)
 func flip(direction):
 	if direction < 0:
-		animated_sprite.flip_h = true
+		animated_sprite.scale.x = -1
 	elif direction > 0:
-		animated_sprite.flip_h = false
+		animated_sprite.scale.x = 1
 
 # Allows the player to slow down time to assist with dodging 
 # bullets while also allowing the player to make it through
 # time slow required sections
-func time_slow(): # perhaps could have respawn timer so as not to waste the players time if they die in slowMo
-	Engine.time_scale = 0.2
-	timeSlowAvailable = false
-	time_slow_length.start()
 
-func _on_time_slow_length_timeout():
-	Engine.time_scale = 1
-	timeSlowAvailable = true
+
 func dash(direction):
 	dashing = true
 	velocity.x = direction * dashSpeed
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -7,9 +7,11 @@ func _on_body_entered(body):
 	if body.has_method("player") && !body.dashing:
 		body.alive = false	# disables player controls
 		timer.start() # respawn timer
+		Engine.time_scale = 1
 	elif body.has_method("enemy"): # if an enemy
 		body.queue_free()
 
 func _on_respawn_timer_timeout():
 	get_tree().reload_current_scene()
+	
 	
