31743f2fbb1356097eb685c822a921045babf1fc
pkemmo4 <pkemmo4@users.noreply.github.com>
2025-01-30 16:38:57 +1100

    Horizontal Movement working

--- scripts/camera_change.gd
+++ scripts/camera_change.gd
@@ -2,7 +2,13 @@ extends Camera2D
 
 # note there is a weird bug where I cannot get rid of 2 in the name of camera2D's direct children
 @onready var timer = $Timer2
-
+@onready var dash_cooldown = $DashCooldown
+@onready var shoot_cooldown = $shootCooldown
+@onready var slowmo_cooldown = $slowmoCooldown
+var scaleChange = Vector2(0.3, 0.3)
+var dashCooldownTime = 0.8
+var shootCooldownTime = 0.5
+var slowMoCooldownTime = 0.8
 func _on_right_boundary_2_body_entered(body):
 	if body.has_method("player") and timer.time_left == 0:
 		position.x += 380
@@ -13,3 +19,25 @@ func _on_left_boundary_2_body_entered(body):
 	if body.has_method("player") and timer.time_left == 0:
 		position.x -= 380
 		timer.start()
+
+func dash():
+	#dash_cooldown.modulate = Color(0.5, 0.5, 0.5)
+	dash_cooldown.scale = dash_cooldown.scale - scaleChange
+	changeScale(dash_cooldown, dashCooldownTime)
+	await get_tree().create_timer(dashCooldownTime).timeout
+	#dash_cooldown.modulate = Color(1, 1, 1)
+
+func shoot():
+	#shoot_cooldown.modulate = Color(0.5, 0.5, 0.5)
+	shoot_cooldown.scale = shoot_cooldown.scale - scaleChange
+	changeScale(shoot_cooldown, shootCooldownTime)
+	await get_tree().create_timer(shootCooldownTime).timeout
+	#shoot_cooldown.modulate = Color(1, 1, 1)
+
+# slowly change the scale to alert the player when they can next use said ability
+func changeScale(object, length): 
+	var increments = length/10
+	var scaleIncrements = scaleChange/10
+	for i in 10:
+		object.scale += scaleIncrements
+		await get_tree().create_timer(increments).timeout
--- scripts/character_movement.gd
+++ scripts/character_movement.gd
@@ -6,20 +6,39 @@ extends CharacterBody2D
 @onready var jump_buffer_timer = $jumpBuffer
 @onready var marker_2d = $AnimatedSprite2D/Marker2D
 @onready var animated_sprite = $AnimatedSprite2D
+signal dashSignal
+signal shootSignal
 const PLAYER_BULLET = preload("res://Scenes/player_bullet.tscn")
 var alive = true
-const DEFAULT_SPEED = 150
-const JUMP_VELOCITY = -300.0
+# Movement
+const maxSpeed = 120
+var onGround
+var desiredVelocity
+var maxSpeedChange
+var acceleration
+var maxAcceleration = 500
+var maxAirAcceleration = 400
+var deceleration 
+var maxDeceleration = 600
+var maxAirDeceleration = 400
+var turnSpeed
+var maxTurnSpeed = 800
+var maxAirTurnSpeed = 600
+const JUMP_VELOCITY = -250.0
+var desiredJump = false
+var newGravity
+var gravityScale = 980
+
 const dashSpeed = 250
 const dashDist = 50
 var dashing = false
 var dashStart = 0
-var speed = DEFAULT_SPEED
 var buffered = 0
 var jumping = false
 const grappleSpeed = 250
 var grappling = false
 var stoppingMomentum = false
+
 
 # when doing the ui use the cooldown timers for each of the abilities using the time_left function
 
@@ -38,7 +57,7 @@ func _physics_process(delta: float) -> void:
 		return
 	# Add gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravityScale * delta
 	
 	if alive == false:
 		velocity.x = 0
@@ -62,7 +81,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote_time.time_left != 0) and !jumping:
 		jump()
 	var direction := Input.get_axis("move_left", "move_right")
-	move(direction)
+	move(direction, delta)
 	flip(direction)
 	if direction != 0 and is_on_floor(): # if not idle
 		animated_sprite.play("running")
@@ -74,16 +93,29 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
+		emit_signal("shootSignal")
 
 func jump():
 	velocity.y = JUMP_VELOCITY
 	jumping = true
 
-func move(direction):
-	if direction:
-		velocity.x = direction * DEFAULT_SPEED
-	elif !grappling: # if grappling keep grappling velocity
-		velocity.x = move_toward(velocity.x, 0, DEFAULT_SPEED)
+func move(direction, delta):
+	desiredVelocity = Vector2(direction, 0) * maxSpeed
+	onGround = is_on_floor()
+	acceleration = maxAcceleration if onGround else maxAirAcceleration
+	deceleration = maxDeceleration if onGround else maxAirDeceleration
+	turnSpeed = maxTurnSpeed if onGround else maxAirTurnSpeed
+	if direction != 0:
+		if (direction < 0 and velocity.x > 0) or (direction > 0 and velocity.x < 0): # if turning
+			maxSpeedChange = turnSpeed * delta
+		else: # if not turning
+			maxSpeedChange = acceleration * delta
+	else: 
+		maxSpeedChange = deceleration * delta
+		
+	velocity.x = move_toward(velocity.x, desiredVelocity.x, maxSpeedChange)
+
+
 func flip(direction):
 	if direction < 0:
 		animated_sprite.scale.x = -1
@@ -102,6 +134,7 @@ func timeSlow():
 func dash():
 	if dashing == false:# only play the dash animation the first time its pressed
 		animated_sprite.play("dash")
+		emit_signal("dashSignal")
 	dashing = true
 	velocity.x = animated_sprite.scale.x * dashSpeed # by using scale.x player still dashes in the direction they are facing when standing still
 	velocity.y = 0
